ab7badb0be06353a1845781c2694e4a34f717c3e
Ahmed Mohamed AbdElMoneam <ahmedabdelmoneam755@gmail.com>
2022-11-15 02:58:36 +0200

    Title Section Completed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 function App() {
   return (
     <div className="App">
-      <h1>Let's start</h1>
+      <div className="container">
+        <div className="title-section">
+          <div className="title-container">
+            <h2 className="title-header">Title Goes here</h2>
+            <p className="title-paragraph">
+              Text paragraph goes here Text paragraph goes here Text paragraph
+              goes here Text paragraph goes here Text paragraph goes here Text
+              Text paragraph goes here
+            </p>
+          </div>
+        </div>
+
+        <div className="content-section">
+          <h1 className="content-header">Your Baraka Pledge</h1>
+        </div>
+      </div>
     </div>
   );
 }
